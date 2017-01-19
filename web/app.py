94b8684f2a233eac9a5e644d1a9740d12c45da00
Cristiano Diedrich <csdiedrich@gmail.com>
2017-01-19 14:50:41 +0000

    envio dos arquivos

---
+++ web/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from redis import Redis
+import os
+
+# Add and initialize Datadog monitoring.
+from datadog import initialize, statsd
+initialize(statsd_host=os.environ.get('DATADOG_HOST'))
+
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    # Increment the Datadog counter.
+    statsd.increment('docker_compose_example.page.views')
+
+    redis.incr('hits')
+    return 'Ola QI! Este Site foi acessado %s vezes.' % redis.get('hits')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
